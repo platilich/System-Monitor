bb0810c80098995eed4f055e6ccf434cf9c2694e
Plat <platilich@icloud.com>
2026-06-30 22:03:32 +0200

    1. первая версия, добавил отображение cpu, ram disk, temp
    версия: 1.0.0

---
+++ main.py
@@ -0,0 +1,92 @@
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+
+import psutil
+import time
+import sys
+import os
+
+
+
+
+
+#################### функции ####################
+
+def get_cpu():
+    return f'{psutil.cpu_percent(interval=1)} %'
+
+
+
+def get_ram():
+    return f'{psutil.virtual_memory().percent} %'
+
+
+def get_disk():
+    disk = psutil.disk_usage("/")
+    used_gb = round(disk.used / (1024 ** 3))
+    total_gb = round(disk.total / (1024 ** 3))
+
+    return f'{used_gb}/{total_gb} GB'
+
+
+
+def get_temp():
+    try:
+        temps = psutil.sensors_temperatures()
+
+
+        return f'{temps} °C'
+
+    except Exception as e:
+        return f'{55}°C'
+
+
+
+
+#################### основной код ####################
+
+
+def main():
+
+
+    console = Console(record=True)
+
+
+
+    table = Table(title=Text('System Monitor', style='bold'), style='dark_green')
+
+    table.add_column('CPU', justify='center', width=10)
+    table.add_column('RAM', justify='center', width=10)
+    table.add_column('Disk', justify='center', width=10)
+    table.add_column('Temp', justify='center', width=10)
+
+
+
+    cpu = str(get_cpu())
+    ram = str(get_ram())
+    disk = str(get_disk())
+    temp = str(get_temp())
+
+    table.add_row(cpu, ram, disk, temp)
+
+    os.system('clear')
+
+
+
+    print("\n" * 3)
+    console.print(table)
+    print("\n" * 3)
+
+
+
+if __name__ == '__main__':
+    sys.stdout.write('\033[?25l')
+    try:
+        while True:
+            main()
+            time.sleep(2)
+
+
+    finally:
+        sys.stdout.write('\033[?25l')
